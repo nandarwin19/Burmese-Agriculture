c424c23a637841dfe12cd9ea678b17eb9c2117ee
nwin19 <nandarwinbuma@gmail.com>
2024-05-19 10:43:00 +0630

    add comments functionality to the profile page

--- client/src/components/common/Comments.jsx
+++ client/src/components/common/Comments.jsx
@@ -1,7 +1,150 @@
-
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Comments() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [comments, setComments] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [commentIdToDelete, setCommentIdToDelete] = useState("");
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getcomments`);
+        const data = await res.json();
+        if (res.ok) {
+          setComments(data.comments);
+          if (data.comments.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchComments();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = comments.length;
+    try {
+      const res = await fetch(
+        `/api/comment/getcomments?startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setComments((prev) => [...prev, ...data.comments]);
+        if (data.comments.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDeleteComment = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(
+        `/api/comment/deleteComment/${commentIdToDelete}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setComments((prev) =>
+          prev.filter((comment) => comment._id !== commentIdToDelete)
+        );
+        setShowModal(false);
+        document.getElementById("my_modal_5").close();
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div>Comments</div>
-  )
+    <div className="">
+      {currentUser.isAdmin && comments.length > 0 ? (
+        <>
+          <div className=" overflow-x-scroll overflow-y-scroll md:overflow-hidden p-3">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Date updated</th>
+                  <th>Comment content</th>
+                  <th>postId</th>
+                  <th>UserId</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+
+              {comments.map((comment) => (
+                <tbody key={comment._id}>
+                  <tr>
+                    <td>{new Date(comment.updatedAt).toLocaleDateString()}</td>
+                    <td>{comment.content}</td>
+                    <td>{comment.postId}</td>
+                    <td>{comment.userId}</td>
+                    <td>
+                      <span
+                        onClick={() => {
+                          document.getElementById("my_modal_5").showModal();
+                          setCommentIdToDelete(comment._id);
+                        }}
+                        className="font-medium text-red-500 hover:underline cursor-pointer"
+                      >
+                        Delete
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          </div>
+        </>
+      ) : (
+        <p>You have no comments yet!</p>
+      )}
+      {showMore && (
+        <button
+          onClick={handleShowMore}
+          className="w-full text-teal-500 self-center text-sm py-7"
+        >
+          Show more
+        </button>
+      )}
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <p className="py-4">Are you sure you want to delete?</p>
+
+          <div className="modal-action">
+            <div className="flex gap-4">
+              <button
+                type="submit"
+                className="btn"
+                onClick={handleDeleteComment}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn"
+                onClick={() => document.getElementById("my_modal_5").close()}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
 }
